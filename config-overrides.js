110c57d998d01af005a076263d746cb3c19ab3a1
verixiaapps <surfer847@outlook.com>
2026-04-29 21:39:27 -0400

    Create config-overrides.js

---
+++ config-overrides.js
@@ -0,0 +1,23 @@
+const webpack = require('webpack');
+
+module.exports = function override(config) {
+  config.resolve.fallback = {
+    crypto: require.resolve('crypto-browserify'),
+    stream: require.resolve('stream-browserify'),
+    buffer: require.resolve('buffer'),
+    process: require.resolve('process/browser'),
+    vm: false,
+    fs: false,
+    path: false,
+    os: false,
+  };
+
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+      process: 'process/browser',
+    })
+  );
+
+  return config;
+};
