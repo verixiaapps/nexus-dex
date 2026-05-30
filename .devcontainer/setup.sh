3f05faa840c0e227d62953bb15451ed76a43841f
verixiaapps <surfer847@outlook.com>
2026-05-30 08:33:13 -0400

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-# Solana CLI 1.18.26
 sh -c "$(curl -sSfL https://release.anza.xyz/v1.18.26/install)"
 export PATH="$HOME/.local/share/solana/install/active_release/bin:$PATH"
 echo 'export PATH="$HOME/.local/share/solana/install/active_release/bin:$PATH"' >> ~/.bashrc
 solana --version
 
-# AVM + Anchor 0.30.1
 cargo install --git https://github.com/coral-xyz/anchor avm --locked --force
 avm install 0.30.1
 avm use 0.30.1
 anchor --version
 
-# Devnet wallet + config
 solana config set --url https://api.devnet.solana.com
 [ -f ~/.config/solana/id.json ] || solana-keygen new --no-bip39-passphrase --silent -o ~/.config/solana/id.json
 solana address
